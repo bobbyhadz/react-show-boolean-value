8e2dfe32df717cde7bbdc9a20986266dbf7e91a8
bobbyhadz <opmftw@gmail.com>
2024-04-07 07:18:34 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function App() {
+  const bool1 = true;
+
+  const bool2 = false;
+
+  return (
+    <div>
+      <h2>bool1: {String(bool1)}</h2>
+
+      <hr />
+
+      <h2>bool2: {String(bool2)}</h2>
+
+      <hr />
+
+      <h2>bobbyhadz.com</h2>
+    </div>
+  );
+}
